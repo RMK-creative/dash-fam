f10f304da6b981ba6d82e661712b313ceb6f66ec
Rachael Kunz <rachaelmkunz@gmail.com>
2021-12-07 17:08:29 +0100

    add basic routing

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/feed", (req, res) => {
+  res.render("feed");
+});
+
+app.get("/profile", (req, res) => {
+  res.render("profile");
+});
+
 app.listen(3080, () => {
   console.log("server is running");
 });
