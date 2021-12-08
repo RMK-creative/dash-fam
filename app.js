9cdb045aeb8f3314ccf2e3288e5c3da7b6506b69
Rachael Kunz <rachaelmkunz@gmail.com>
2021-12-08 14:41:22 +0100

    add database connection, route for createPost & updated listen

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const app = express();
+const connectDB = require("./config/database");
 
+require("dotenv").config({ path: "./config/.env" });
+
+connectDB();
+
+// view engine
+app.set("view engine", "ejs");
+
+// MIDDLEWARE
 app.use(express.static("public"));
 
-app.set("view engine", "ejs");
+// ROUTES
+app.use("/post/createPost", require("./routes/posts"));
 
 app.get("/", (req, res) => {
   res.render("index");
@@ -17,6 +27,9 @@ app.get("/profile", (req, res) => {
   res.render("profile");
 });
 
-app.listen(3080, () => {
-  console.log("server is running");
-});
+const PORT = process.env.PORT || 3050;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on ${PORT}`)
+);
