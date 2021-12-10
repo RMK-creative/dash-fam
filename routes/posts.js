c13ac2968bc73836af7bc6212d6882bb9ff33591
Rachael Kunz <rachaelmkunz@gmail.com>
2021-12-10 15:13:08 +0100

    add delete

--- routes/posts.js
+++ routes/posts.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const postsController = require("../controllers/posts");
 
 router.post("/createPost", postsController.createPost);
+router.delete("/deletePost/:id", postsController.deletePost);
 
 module.exports = router;
