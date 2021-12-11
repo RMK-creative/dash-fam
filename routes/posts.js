17ffa5d1e9fc9ecce44a6f75ebaaf6929a6e1c75
Rachael Kunz <rachaelmkunz@gmail.com>
2021-12-11 12:02:28 +0100

    add require multer & update router posts to include upload.single file

--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const postsController = require("../controllers/posts");
+const upload = require("../middleware/multer");
 
-router.post("/createPost", postsController.createPost);
+router.post("/createPost", upload.single("file"), postsController.createPost);
 router.put("/likePost/:id", postsController.likePost);
 router.delete("/deletePost/:id", postsController.deletePost);
 
